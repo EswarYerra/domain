6b014a8f5ea07b4e0e24ecd8b9e8aa08feb50727
eswar <eswaryerra530@.com>
2025-12-01 14:49:04 +0530

    Your update messagess

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -71,6 +71,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
+    "https://frontend-966s.onrender.com",
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
